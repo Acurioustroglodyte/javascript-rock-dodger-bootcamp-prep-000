c5cda4e2ecd787a8fd334342c510df9576826a16
Trevor Jameson <tjameson15@gmail.com>
2018-06-12 18:05:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -125,7 +125,9 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
-  ROCKS = []
+  for (let i = 0; i < ROCKS.length; i++) {
+    ROCKS[i].remove();
+  }
   alert("YOU LOSE!");
   window.removeEventListener('keydown', moveDodger()) // Should moveDodger be called with ()?
 }
